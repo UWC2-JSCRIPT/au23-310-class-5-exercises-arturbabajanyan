74c481e608d0c3c5b58bb3c238b85f8f7e06413b
arturbabajanyan <arturbabajanyanv@gmail.com>
2023-11-11 21:39:22 -0500

    Plus and Minus is done with html involved

--- plusesAndMinuses.js
+++ plusesAndMinuses.js
@@ -1,2 +1,15 @@
 // When a user clicks the + element, the count should increase by 1 on screen.
+
+let headerEl = document.getElementById('h1El');
+
+const plusEl = document.getElementById('plus');
+plusEl.addEventListener('click', () => {
+    headerEl.innerHTML = (+headerEl.innerHTML + 1);
+});
+
 // When a user clicks the – element, the count should decrease by 1 on screen.
+
+const minusEl = document.getElementById('minus');
+minusEl.addEventListener('click', () => {
+    headerEl.innerHTML = (+headerEl.innerHTML - 1);
+});
