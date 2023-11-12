de63bc858c5a5388a454a43d62e1546d1711e8bf
arturbabajanyan <arturbabajanyanv@gmail.com>
2023-11-12 01:23:37 -0500

    Where is the pointer is done and it writes in each td individually

--- wheresThePointer.js
+++ wheresThePointer.js
@@ -1,3 +1,12 @@
 // Attach one listener that will detect clicks on any of the <td>
 // elements.  Should update that element's innerHTML to be the
 // x, y coordinates of the mouse at the time of the click
+
+const allCubes = document.querySelectorAll('td');
+
+for (let i = 0; i < allCubes.length; i++) {
+    allCubes[i].addEventListener('click', function(e) {
+        allCubes[i].innerHTML = `X = ${e.clientX}<br/>Y = ${e.clientY}`
+        console.log(e.clientX, e.clientY);
+    })
+};
