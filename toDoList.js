628d12037fe80ba7526ed3010be5223bd0bb2688
arturbabajanyan <arturbabajanyanv@gmail.com>
2023-11-14 19:01:01 -0500

    toDoList Done

--- toDoList.js
+++ toDoList.js
@@ -1,14 +1,39 @@
-// If an li element is clicked, toggle the class "done" on the <li>
+let ulElement = document.querySelector('ul.today-list');
 
-// If a delete link is clicked, delete the li element / remove from the DOM
+ulElement.addEventListener(('click'), (e) => {
+	if (e.target.nodeName.toLowerCase() == 'span') {
+		e.target.parentNode.classList.toggle('done');
+	}
+	if (e.target.className == 'delete') {
+		e.target.parentElement.remove();
+	}
+})
 
 // If an 'Add' link is clicked, adds the item as a new list item with
 // addListItem function has been started to help you get going!
 // Make sure to add an event listener(s) to your new <li> (if needed)
-const addListItem = function(e) {
-  e.preventDefault();
-  const input = this.parentNode.getElementsByTagName('input')[0];
-  const text = input.value; // use this text to create a new <li>
+const addListItem = function (e) {
+	e.preventDefault();
+	const input = this.parentNode.getElementsByTagName('input')[0];
+	const text = input.value; // use this text to create a new <li>
 
-  // Finish function here
-};
+  if (input.value != '') {
+
+    const newLiEl = document.createElement('li');
+    const newAEl = document.createElement('a');
+    newAEl.className = 'delete';
+    newAEl.textContent = 'Delete';
+  
+    const newSpanItem = document.createElement('span');
+    newSpanItem.textContent = `${text}\n`;
+  
+    newLiEl.append(newSpanItem, newAEl);
+    ulElement.appendChild(newLiEl);
+  
+    input.value = '';
+    
+  }
+}
+
+const addItem = document.querySelector('a.add-item');
+addItem.addEventListener('click', addListItem);
